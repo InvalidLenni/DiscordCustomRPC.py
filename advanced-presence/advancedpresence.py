e4670a1c071229f8e2c4648fce035ea012780e54
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-01-30 15:49:56 +0100

    Update advancedpresence.py

--- advanced-presence/advancedpresence.py
+++ advanced-presence/advancedpresence.py
@@ -1,15 +1,15 @@
 from pypresence import Presence
 import time
 
-client_id = '850644889423446026'
+client_id = '[VAR.clientid]'
 RPC = Presence(client_id)
 RPC.connect()
 
-RPC.update(details="Hi there, i'm Lenni!",
-           large_image='coding',
-           large_text='invalidlenni.de',
-           small_image='avatar',
-           small_text='lenni',
+RPC.update(details="[VAR.details]",
+           large_image='[VAR.largeImageKey]',
+           large_text='[VAR.largeImageText]',
+           small_image='[VAR.smallImageKey]',
+           small_text='[VAR.smallImageText]',
            buttons=[{"label": "Github", "url": "https://github.com/InvalidLenni/", "Website", "https://invalidlenni.de/"}],
            start=time.time())
 print('Online')
